6cd6409733cf4d806065411a22c8c7711d6d5462
Fernando <lkgfernando@gmail.com>
2026-04-27 21:12:27 -0300

    feat: first commit

---
+++ tipagem_dados/variaveis.ts
@@ -0,0 +1,33 @@
+//Sintaxe básica
+let nome: string = "Fernando";
+let idade: number = 41;
+let ativo: boolean = true;
+let salario: number = 5500.50;
+
+
+//Tipo primitivos
+//String textos
+let empresa: string = "XPTO"
+let cargo: string = "DevOps/SRE"
+
+//Numbers - números (inteiros)
+let qtdServidores: number = 5;
+let uptime: number = 99.9;
+
+//Null e Undefined
+let valorNulo: null = null;
+let valorIndefinido: undefined = undefined;
+
+/*-------------------------------------*
+| Inferências de tipo                  |
+*--------------------------------------*/
+
+let cidade = "Artur Nogueira"; // TypeScript sabe que é string
+let temperatura = 25; // TypeScript sabe que é number
+
+// Mesmo sem declarar explicitamente, ele protege:
+//temperatura = "quente"; // ERRO!
+
+
+
+
