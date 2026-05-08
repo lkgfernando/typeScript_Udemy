527c4dccb020dfacd0f916b3b38b281efb5756c2
Fernando <lkgfernando@gmail.com>
2026-05-07 22:42:27 -0300

    Type-annotation

---
+++ src/type-annotation/type-annotation.ts
@@ -0,0 +1,29 @@
+//tipos básicos
+let nome: string = 'Fernanod';
+let idade: number = 25;
+let programador: boolean = true;
+let simbolo: symbol = Symbol('qualquer-simbolo');
+
+
+//Arrays
+let arrayDeNumeros: Array<number> = [1, 2, 3];
+let arrayDeStrings: Array<string> = ['a', 'b', 'c'];
+let arrayDeNumeros2: number[] = [1, 2, 3];
+let arrayDeStrings2: string[] = ['a', 'b', 'c'];
+
+//Objetos
+let pessoa: {nome: string, idade: number, programador?: boolean} = {
+    nome: 'Fernanod',
+    idade: 25,
+    programador: true,
+}
+
+// Funcoes
+
+function soma(a: number, b: number): number {
+    return a + b;
+}
+
+const soma2: (x: number, y: number) => number = (x, y) => x + y;
+
+
